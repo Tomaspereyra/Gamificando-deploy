a335ef3197289db15c823a007b076de59e30d2aa
Tomaspereyra <pereyratomas18@gmail.com>
2020-03-01 19:45:57 -0300

    no message

--- gamificandoalgoritmos/src/main/java/com/unla/gamificandoalgoritmos/rest/WebController.java
+++ gamificandoalgoritmos/src/main/java/com/unla/gamificandoalgoritmos/rest/WebController.java
@@ -1,15 +1,17 @@
 package com.unla.gamificandoalgoritmos.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebController {
 	
-	@RequestMapping("/hello")
+	@GetMapping("/hello")
 	public String test() {
 		return "hello";
 	}
+	
 	
 
 }
